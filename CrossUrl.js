f03ae338c6ae601e28372b73714eafe4a89589cc
Sejun Jeong <mailiam@gmail.com>
2013-03-27 04:02:19 +0900

    INITIAL CrossUrl

---
+++ CrossUrl.js
@@ -0,0 +1,63 @@
+function CrossUrl (elem, urls, options) {
+	if ( !elem ) {
+      console.error('Element not found.')
+      return;
+    }
+
+    this.element = elem;
+    this.urlList = urls;
+
+	this.options = {};
+
+	for ( var prop in CrossAppUrl.defaults ) {
+  		this.options[ prop ] = CrossAppUrl.defaults[ prop ];
+	}
+
+	for ( prop in options ) {
+      this.options[ prop ] = options[ prop ];
+    }
+
+    this._create();
+}
+
+CrossAppUrl.defaults = {
+    timeout:500,
+    message:"Sorry, The device is not yet supported."
+};
+
+CrossAppUrl.prototype = {
+	_create: function(){
+		var checker = this;
+		checker.checkframe = document.createElement('iframe');
+		checker.checkframe.style.display = 'none';
+		var _body = document.getElementsByTagName('body') [0];
+		
+		this.element.addEventListener('click', function(){
+			_body.appendChild(checker.checkframe);
+			checker.tryURL();
+			_body.removeChild(checker.checkframe);
+		}, false);
+	},
+
+	tryURL: function(i) {
+		i = typeof i !== 'undefined' ? i : 0;
+
+        var now = new Date();
+        var time = now.getTime();
+        var checker = this;
+        
+        setTimeout(function() {
+                   var now2 = new Date();
+                   var time2 = now2.getTime();
+                   if(time2 - time < checker.options.timeout*2) {
+                        if(i+1<checker.urlList.length){
+                            checker.tryURL(i+1)
+                        }else{
+                            alert(checker.options.message);
+                            return;
+                        }
+                   }
+                   }, this.timeout);
+        checker.checkframe.location = this.urlList[i];
+    }
+}
